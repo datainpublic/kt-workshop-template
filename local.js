cb3bad2f53a1008f7cf5e1524be5e7a8d5d0050b
jyxie <j.xie19@gmail.com>
2019-06-06 21:01:48 -0400

    Update local.js

--- local.js
+++ local.js
@@ -56,7 +56,7 @@ var duration = moment.duration(between_now_and_event).minutes();
 const marquee_message =
   `<li id="xkcd"><marquee>
 
-  1.5˚ Climate Crisis ${years_to_event} years <span id="minutes" class='part'>${duration}</span>min
+7 days until three more people will die in prison
 
   </marquee></li>`;
 
